8da16176c4dfbdf4da5bc580e39f7fe698c27f7f
Pheeraphat Dherachaisuphakij <107838345+Pheeraphat47@users.noreply.github.com>
2023-10-30 19:28:14 +0700

    Update ProductManagement.js

--- ProductManagement.js
+++ ProductManagement.js
@@ -1,29 +1,30 @@
 function ProductManagement() {
   //add products variable here
   
-  let products = [];
-  let currentId = 1;
-
-
+  const products = [];
+  
   function getAllProducts() {
     // products.forEach((element) => console.log(element));
     return products;
   }
-
+    // add new product to products(array)
   function addProduct(product) {
       return products.push(product)
   }
 
+  // search product by name
   function searchByName(name) {
       return products.filter((product) => product.name == name )
   }
-
+  
+  // search product by price range between min to max
   function searchByPriceRange(minPrice, maxPrice) {
      return products.filter((product) => product.price >= minPrice && product.price <= maxPrice)
   }
 
+  // remove all product
   function removeAll() {
-    return products = []
+    return products.length = 0;
   }
 
   return {
